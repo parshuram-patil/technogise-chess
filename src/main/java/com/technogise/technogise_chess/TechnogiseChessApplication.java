ce5c4bc0a058dd8475d9735e1eab5abf6faec523
Parshuram Patil <parshuram.patil@outlook.in>
2025-02-06 18:29:58 +0530

    initial commit

---
+++ src/main/java/com/technogise/technogise_chess/TechnogiseChessApplication.java
@@ -0,0 +1,13 @@
+package com.technogise.technogise_chess;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TechnogiseChessApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TechnogiseChessApplication.class, args);
+	}
+
+}
